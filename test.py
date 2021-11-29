eda41c2a2ad1732fb9d7f64abb6e9b3af73dd95a
liW-J <2990117179@qq.com>
2021-11-30 01:06:08 +0800

    增加题目类型说明

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 # import csv
 #
 # headers = ['class','name','sex','height','year']
@@ -13,10 +14,91 @@
 #     f_csv = csv.writer(f)
 #     f_csv.writerow(headers)
 #     # f_csv.writerows(rows)
-data="我不知道不知道不知道"
+# data="我不知道不知道不知道"
 # data=data.replace("不知","嘿嘿",3)
 # print(data)
-for i in range(5):
-    print(i)
-    data = data.replace("不知", '__{}__'.format(i))
-    print(data)
+# for i in range(5):
+#     print(i)
+#     data = data.replace("不知", '__{}__'.format(i))
+#     print(data)
+
+# with open("./中医疾病与病征编码.txt", 'r', encoding='utf8', newline='') as file:
+#     for i in file:
+#         i.strip('\n')
+#         print(i)
+#         break
+
+# with open("./中医疾病与病征编码.txt", 'r', encoding='utf8', newline='') as f:
+#     txt = []
+#     for line in f.readlines():
+#         curline=line.strip().split(" ")
+#         txt.append(curline[0])ew
+# print(txt)
+
+# with open("LevelTwo.csv", 'r', encoding='utf8', newline='') as f:
+#     word = []
+#     for line in f.readlines():
+#         curline = line.strip().split(" ")
+#         word.append(curline[0])
+# #     print(word)
+# from function import disturb
+# new=disturb("LevelTwo.csv", "碎石")
+# print(new)
+# new1=disturb("LevelTwo.csv", "碎石")
+# print(new1)
+# with open("./key/1.1key2.csv","r",encoding='utf8') as f4:
+#     R=f4.readlines()
+#     for i in range(len(R)):
+#         print(R[i])
+# from ltp import LTP
+# encoding: UTF-8
+import re
+# txt="bcd"
+# 将正则表达式编译成Pattern对象
+# pattern = re.compile(r'hello')
+
+# 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
+# match = pattern.match('hello world!')
+
+# if match:
+    # 使用Match获得分组信息
+    # print(match.group())
+# print(re.sub("b+","#",txt))
+# print(txt)
+# print(result)
+# ### 输出 ###
+# # hello
+#
+# a=["1","2","3"]
+# if "1" in a:
+#     a.remove("2")
+# print(a)
+# ltp = LTP()
+# for i in ["这么","我"]:
+#     seg, hidden = ltp.seg([i])
+#     pos = ltp.pos(hidden)
+#     print(pos)
+#     if pos[0][0]=='v':
+#         print(True)
+from function import dig
+count=1
+LevelOne=0
+key=[]
+result1=["我喝"]
+listLine=["我喝"]
+word={"我喝":0}
+num=0
+data="{'history': ['患者：前表性胃炎，__ 我喝纯牛奶好不好？养胃吗（男，22岁）', '医生：平时有什么不舒服吗？如果没有特别的，不用吃药，饮食上注意就可以。纯牛奶可以的。', '患者：平时吃一碗多饭胃又涨了，平时有点痛，我每天都会喝纯牛奶的。', '医生：去做胃镜了吗？', '患者：今年没有做，还是去年做的，', '医生：恩，应该没事，如果厉害的话，可以吃点奥美拉唑。', '医生：或者可以少食多餐，易消化的食物，多运动。', '患者：为什么老是打嗝。', '医生：有可能胀气。你喝了奶后，会厉害吗？', '患者：一样的。', '医生：那你先别喝牛奶了，牛奶容易胀气。', '医生：豆类食品最好先别吃。', '医生：平时可以多喝点小米稀饭，这个养胃，吃完饭不要立刻躺下。', '患者：豆类我先不吃，牛奶还要喝的，纯牛奶喝了没事，我只对酸奶有，喝酸奶会反酸。', '医生：嗯呢，那你试试。', '患者：每次都是一个小时后才躺下了，真是遭罪。', '医生：平时吃饭，也要注意，那些吃了不舒服，以后就少吃点。', '医生：慢性胃炎，都要靠慢慢的养。', '患者：好的太慢了，人都消瘦了，']}"
+# data = data.replace(a, ' __' + str(count) + '__ ', 1)
+
+# LevelOne,count,data=dig( 0, LevelOne, data, key, word,count,3, result1,listLine)
+
+
+print(data)
+a="__ "+'['+listLine[num]+']'
+print(a)
+if re.sub(a,"#",data)==data:
+    print(True)
+else:
+    print(False)
+data = data.replace(listLine[0], ' __' + str(count) + '__ ',1)
--- word.py
+++ word.py
@@ -1,21 +1,87 @@
-
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml.ns import qn
+from docx import Document
+from docx.shared import Pt
+from function import option
+
+import random,time
+#修改六处：①textQueation.txt;②clear.txt;③标题；④clear.txt;⑤key.txt;⑥output.txt
 
 from time import strftime, localtime
-timestr=strftime('%Y-%m-%d %H:%M:%S',localtime())
+#清理数据！
+with open("question/textQuestion2.txt", "r", encoding='utf8') as f1:
+    with open("./clear/clear.csv","a",encoding='utf8') as f2:
+        for line in f1:
+            str1=line.lstrip("{/'history/': [ ")
+            str2=str1.rstrip("\']}")
+
+            f2.write(str2)
+            # print(str2)
 
 doc1 = Document()  #生成一个空的docx对象
-head=doc1.add_heading('安全测试题', level=1) # 添加标题
-head.alignment = WD_ALIGN_PARAGRAPH.CENTER  # 居中
-p1=doc1.add_paragraph('edit by:HuaBro \t update time: {}'.format(timestr))   # 添加段落
-p1.alignment = WD_ALIGN_PARAGRAPH.CENTER  # 居中
+head=doc1.add_paragraph() # 添加标题
+head.add_run('二、阅读').font.bold = True # 字形加粗
+head.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # 居中
+head.size = Pt(36)
+head1=doc1.add_paragraph()
+head1.add_run('第一部分').font.bold = True # 字形加粗
+head1.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # 居中
+head1.size = Pt(20)
+doc1.add_paragraph('选择正确的词语填空') # 添加标题
+# p1=doc1.add_paragraph('edit by:HuaBro \t update time: {}'.format(timestr))   # 添加段落
+# p1.alignment = WD_ALIGN_PARAGRAPH.CENTER  # 居中
 
 
-for i,j in zip(queslst,optlst):
-    doc1.add_paragraph(i)
-    doc1.add_paragraph(j)
-doc1.styles['Normal'].font.name = '宋体'
-doc1.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体') # 字体
-doc1.styles['Normal'].font.name = 'Times New Roman' # 数字字体
-doc1.save('output1.docx')
+with open("./clear/clear.csv","r",encoding='utf8') as f:
+    with open("key/key2.csv", "r", encoding='utf8') as f4:
+        with open("newKey/newKey2.csv", "r", encoding='utf8') as f5:
+            KEY=f5.readlines()
+            # Right=f4.readlines()
+            keycount=0
+            count=0
+            for i,j in zip(f,f4):
+                print(i)
+                str1=i.split("', '")
+                str2=j.split(",")
+                keycount+=len(str2)
+                count+=1
+                print(str1)
+                if len(str1)>10:
+                    continue
+                # print(str2)
+                #将题目写入docx
+                for k in str1:
+                    if k == str1[-1]:
+                        k = k.strip("']}\n")
+                    doc1.add_paragraph(k)
+                print(k)
+                for num in range(len(KEY)):
+
+                    if num<keycount and num>=(keycount-len(str2)):
+
+                        print(KEY[num])
+                        NEWKEY=KEY[num].split(',')
+                        print(NEWKEY)
+
+                        for k in NEWKEY:
+                            if k == NEWKEY[-1]:
+                                k = k.strip('\n')
+                                NEWKEY[-1] = k
+                                print(NEWKEY)
+                        if len(NEWKEY) == 5:
+
+                            A,B,C,D,RightKey=option(NEWKEY)
+
+                            doc1.add_paragraph("{}.A.{}B.{}C.{}D.{}\t\t正确答案：{},{}".format(len(str2)-(keycount-num)+1,A, B, C, D,RightKey,NEWKEY[-1]))
+
+                        else:
+                            A = NEWKEY[0]
+                            doc1.add_paragraph("{}. A.{}\t\t正确答案：{}".format(len(str2)-(keycount-num)+1,A,NEWKEY[0]))
+     # for l in str2:
+                #     doc1.add_paragraph(l)
+
+                # doc1.add_paragraph(j)
+        doc1.styles['Normal'].font.name = '宋体'
+        doc1.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体') # 字体
+        doc1.styles['Normal'].font.name = 'Times New Roman' # 数字字体
+        doc1.save('./output/output2.docx')
